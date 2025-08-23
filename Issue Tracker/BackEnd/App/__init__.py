d2314240d215b3fadd0d067ad2aba9e4e487f4c7
Apue <apue2434@gmail.com>
2025-08-23 14:14:24 +0800

    Initialized dotenv file, Havent tested backend

--- Issue Tracker/BackEnd/App/__init__.py
+++ Issue Tracker/BackEnd/App/__init__.py
@@ -2,23 +2,28 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from pathlib import Path
-import logging
+from dotenv import load_dotenv
+import logging, os
+
 
 db = SQLAlchemy()
-DB_NAME = "IssueTracker.db"
+DB_NAME = os.getenv("DB_NAME")
 
 logging.basicConfig(level=logging.info, filename="Main.log", filemode="w", 
                     format="%(filename)s - %(asctime)s - %(levelname)s - %(message)s",
                     datefmt="%Y-%M-%D %H:%M:%S")
+
+env_path = Path(__file__).resolve().parent.parent.parent / ".env"
+load_dotenv(dotenv_path=env_path)
 
 logger = logging.getLogger(__name__)
 
 def run_app():
     app = Flask(__name__)
 
-    app.config("SQLALCHEMY_DATABASE_URI") = f"sqlite:///{DB_NAME}"
-    app.config("SECRET_KEY") = "4f7690667988b8124afae2b90bd62432"
-    app.config("SQLALCHEMY_TRACK_MODIFICATIONS") = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     CORS(app)
 
